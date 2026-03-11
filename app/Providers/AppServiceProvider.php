8623c7c5845bf09e3166ab6e6c54c79df4c363da
mohamed kamal <mo7amedkamal3wad@gmail.com>
2026-03-11 16:16:59 +0200

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Setting;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,9 +17,13 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     { 
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+        
         Paginator::useBootstrapFive();
         view()->composer('*', function ($view) {
-        $view->with('setting', Setting::first());
-    });
+            $view->with('setting', Setting::first());
+        });
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,7 +15,7 @@
       type="image/x-icon" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/login.css') }}">
 </head>
 
 <body>
@@ -180,3 +180,4 @@
 </body>
 
 </html>
+
